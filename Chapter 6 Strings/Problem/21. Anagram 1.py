3e990bc7ae6fd892cf6939198884e6fc2ecd59e3
Long0Amateur <70660630+Long0Amateur@users.noreply.github.com>
2020-09-06 14:09:06 -0400

    Delete 21. Anagram 1.py

--- Chapter 6 Strings/Problem/21. Anagram 1.py
+++
@@ -1,9 +0,0 @@
-
-import random
-
-def anagram(value):
-    '''Returns random anagram of given value'''
-    return ''.join(random.sample(value, len(value)))
-
-s = input('Enter a string:')
-print(anagram(s))
