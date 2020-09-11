79ab557b99dd4da75024182014e043a8b24fd18b
Long0Amateur <70660630+Long0Amateur@users.noreply.github.com>
2020-09-10 23:46:02 -0400

    Update and rename Chapter 8 More with Lists/2D list/A program that generates 2D list.py to Chapter 8 More with Lists/2D list/A program that generates 2D list and counts odd/even.py

---
+++ Chapter 8 More with Lists/2D list/A program that generates 2D list and counts odd/even.py
@@ -0,0 +1,17 @@
+# A program that generates 2D list and counts how many odd/even
+
+from pprint import pprint
+
+r = 5
+c = 6
+
+# Remember: L[i][j] = i*j
+
+L = [ [i*j for j in range(c)] for i in range(r)]
+
+pprint(L)
+
+count_odd = sum( [1 for r in range(10) for c in range(5) if L[r][c]%2 != 0])
+
+print('Odd:%d' %count_odd)
+print('Even:', (len(L) * len(L[0]))- count_odd )
